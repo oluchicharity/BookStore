e95d1a348db5388ef74f1682da95bdd41d2650a0
Agbakwuru Oluchi <lulu@Agbakwurus-MacBook-Air.local>
2024-05-07 14:12:28 +0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { ConnectOptions } from 'mongoose';
+// import userRouter from './routes/userRoutes';
+// import postRouter from "./routes/postRoutes"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Application = express();
+const PORT = process.env.PORT || 1234;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to BookStore');
+});
+
+app.use(express.json());
+
+
+// app.use('/api/v1', userRouter,postRouter);
+
+
+const mongodb = 'mongodb+srv://agbakwuruoluchi29:XjrsTUWxbFVSGNJ0@cluster0.tffuwpj.mongodb.net/BookStore';
+mongoose.connect(mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+} as ConnectOptions).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+// XjrsTUWxbFVSGNJ0
